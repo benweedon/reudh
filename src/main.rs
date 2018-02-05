2ffef2041bbc93065aa7468577fb52fb9658e8b4
Ben Weedon <ben.weedon@outlook.com>
2018-02-04 23:23:08 -0800

    Use html5ever to parse HTML, then spit back out to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 extern crate futures;
+extern crate html5ever;
 extern crate hyper;
 extern crate hyper_tls;
 extern crate tokio_core;
 
-use std::io::{self, Write};
+use std::io;
 
 use futures::{Future, Stream};
+use html5ever::{parse_document, serialize};
+use html5ever::driver::ParseOpts;
+use html5ever::rcdom::RcDom;
+use html5ever::tendril::TendrilSink;
 use hyper::Client;
 use hyper_tls::HttpsConnector;
 use tokio_core::reactor::Core;
@@ -19,8 +24,17 @@ fn main() {
     let uri = "https://www.etymonline.com/".parse().unwrap();
 
     let work = client.get(uri).and_then(|res| {
-        res.body()
-            .for_each(|chunk| io::stdout().write_all(&chunk).map_err(From::from))
+        res.body().concat2().and_then(|body| {
+            let opts = ParseOpts {
+                ..Default::default()
+            };
+            let dom = parse_document(RcDom::default(), opts)
+                .from_utf8()
+                .read_from(&mut &*body)
+                .unwrap();
+            serialize(&mut io::stdout(), &dom.document, Default::default()).unwrap();
+            Ok(())
+        })
     });
 
     core.run(work).unwrap();
