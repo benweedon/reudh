cd1e5720649d535aa8213a3286eeae0249ccb2f5
Ben Weedon <ben.weedon@outlook.com>
2018-02-04 22:40:22 -0800

    Get HTML of https://www.etymonline.com with hyper
    I'm not really doing anything with the HTML right now, just getting it.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate futures;
+extern crate hyper;
+extern crate hyper_tls;
+extern crate tokio_core;
+
+use std::io::{self, Write};
+
+use futures::{Future, Stream};
+use hyper::Client;
+use hyper_tls::HttpsConnector;
+use tokio_core::reactor::Core;
+
 fn main() {
-    println!("Hello, world!");
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+    let client = Client::configure()
+        .connector(HttpsConnector::new(4, &handle).unwrap())
+        .build(&handle);
+    let uri = "https://www.etymonline.com/".parse().unwrap();
+
+    let work = client.get(uri).and_then(|res| {
+        res.body()
+            .for_each(|chunk| io::stdout().write_all(&chunk).map_err(From::from))
+    });
+
+    core.run(work).unwrap();
 }
