e4ed7a8a5f82cbdf718fa7194022d202c1d872c9
Ben Weedon <ben.weedon@outlook.com>
2018-02-10 00:54:43 -0800

    Multithread fetching
    There are definitely still lots of performance improvements to be made
    on this front, but this is a good start.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+extern crate chan;
 extern crate futures;
 extern crate html5ever;
 extern crate hyper;
@@ -5,10 +6,13 @@ extern crate hyper_tls;
 extern crate indicatif;
 extern crate kuchiki;
 extern crate native_tls;
+extern crate num_cpus;
 extern crate tokio_core;
 
 mod errors;
 mod parse;
+
+use std::thread;
 
 use errors::Error;
 
@@ -37,23 +41,49 @@ impl Iterator for PageIter {
 }
 
 pub fn fetch() -> Result<(), Error> {
-    let mut etyms = vec![];
     let bar = ProgressBar::new(26);
     bar.set_style(ProgressStyle::default_bar().template(
         "{prefix}\n[{elapsed_precise}/{eta_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
     ));
     bar.set_prefix("fetching...");
 
-    let pages = PageIter::new();
-    for url in pages {
-        let e = parse::etyms_from_letter_url(url)?;
-        etyms.extend(e);
-        bar.inc(1);
-    }
-    bar.finish_and_clear();
+    let (page_sender, page_receiver) = chan::sync(1);
+    let (etym_sender, etym_receiver) = chan::sync(1);
+    let (bar_sender, bar_receiver) = chan::sync(1);
 
-    for etym in etyms {
+    thread::spawn(move || {
+        let pages = PageIter::new();
+        for url in pages {
+            page_sender.send(url);
+            bar.inc(1);
+        }
+        bar_sender.send(bar);
+    });
+    for _ in 0..num_cpus::get() {
+        let page_receiver = page_receiver.clone();
+        let etym_sender = etym_sender.clone();
+        thread::spawn(move || loop {
+            match page_receiver.recv() {
+                Some(url) => {
+                    let etyms = parse::etyms_from_letter_url(url).unwrap();
+                    for etym in etyms {
+                        etym_sender.send(etym);
+                    }
+                }
+                None => return,
+            }
+        });
+    }
+    drop(page_receiver);
+    drop(etym_sender);
+
+    for etym in etym_receiver {
         println!("{}\n---------------------\n\n", etym);
     }
+    let bar = bar_receiver
+        .recv()
+        .ok_or(Error::new("Progress bar not received"))?;
+    bar.finish_and_clear();
+
     Ok(())
 }
