175a665fd1f630a4e1bf4b0977b1e36d6df037f5
Ben Weedon <ben.weedon@outlook.com>
2018-02-09 17:59:12 -0800

    Add progress bar

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ extern crate futures;
 extern crate html5ever;
 extern crate hyper;
 extern crate hyper_tls;
+extern crate indicatif;
 extern crate kuchiki;
 extern crate native_tls;
 extern crate tokio_core;
@@ -11,6 +12,8 @@ mod parse;
 
 use errors::Error;
 
+use indicatif::{ProgressBar, ProgressStyle};
+
 pub fn fetch() -> Result<(), Error> {
     const LETTERS: &'static str = "abcdefghijklmnopqrstuvwxyz";
 
@@ -19,10 +22,17 @@ pub fn fetch() -> Result<(), Error> {
         .map(|l| format!("https://www.etymonline.com/search?q={}", l));
 
     let mut etyms = vec![];
+    let bar = ProgressBar::new(26);
+    bar.set_style(ProgressStyle::default_bar().template(
+        "{prefix}\n[{elapsed_precise}/{eta_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
+    ));
+    bar.set_prefix("fetching...");
     for url in letter_urls {
         let e = parse::etyms_from_letter_url(url)?;
         etyms.extend(e);
+        bar.inc(1);
     }
+    bar.finish_and_clear();
 
     for etym in etyms {
         println!("{}\n---------------------\n\n", etym);
