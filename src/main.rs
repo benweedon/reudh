21416117526ad209b3b93afa070a88362124d11b
Ben Weedon <ben.weedon@outlook.com>
2018-02-09 20:42:59 -0800

    Add command line parsing with clap
    Fetching data is now done through the fetch subcommand.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+#[macro_use]
+extern crate clap;
 extern crate reudh;
 
+use clap::{App, AppSettings};
+
 fn main() {
-    println!("Version {}\n", env!("CARGO_PKG_VERSION"));
-    match reudh::fetch() {
-        Ok(_) => println!("\nSuccess!"),
-        Err(err) => eprintln!("\nFailure: {}", err),
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml)
+        .name(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .get_matches();
+
+    if let Some(_) = matches.subcommand_matches("fetch") {
+        match reudh::fetch() {
+            Ok(_) => println!("\nSuccess!"),
+            Err(err) => eprintln!("\nFailure: {}", err),
+        }
     }
 }
